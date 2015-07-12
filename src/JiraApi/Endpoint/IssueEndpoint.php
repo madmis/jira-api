f3261d40ec8ac380e1c2544e217595aff6582a17
madmis <madmis@inbox.ru>
2015-07-12 14:12:05 +0300

    mapping

--- src/JiraApi/Endpoint/IssueEndpoint.php
+++ src/JiraApi/Endpoint/IssueEndpoint.php
@@ -10,24 +10,6 @@ use madmis\JiraApi\Model\Issue;
 /**
  * Class IssueEndpoint
  * @package madmis\JiraApi\Endpoint
- *
- * http://example.com:8080/jira/rest/api/2/issue [POST]
- * http://example.com:8080/jira/rest/api/2/issue/bulk [POST]
- * http://example.com:8080/jira/rest/api/2/issue/{issueIdOrKey} [GET, DELETE, PUT]
- * http://example.com:8080/jira/rest/api/2/issue/{issueIdOrKey}/assignee [PUT]
- * http://example.com:8080/jira/rest/api/2/issue/{issueIdOrKey}/comment [GET, POST]
- * http://example.com:8080/jira/rest/api/2/issue/{issueIdOrKey}/comment/{id} [GET, PUT, DELETE]
- * http://example.com:8080/jira/rest/api/2/issue/{issueIdOrKey}/editmeta [GET]
- * http://example.com:8080/jira/rest/api/2/issue/{issueIdOrKey}/notify [POST]
- * http://example.com:8080/jira/rest/api/2/issue/{issueIdOrKey}/remotelink [GET, POST, DELETE]
- * http://example.com:8080/jira/rest/api/2/issue/{issueIdOrKey}/remotelink/{linkId} [GET, PUT, DELETE]
- * http://example.com:8080/jira/rest/api/2/issue/{issueIdOrKey}/transitions [GET, POST]
- * http://example.com:8080/jira/rest/api/2/issue/{issueIdOrKey}/votes [DELETE, POST, GET]
- * http://example.com:8080/jira/rest/api/2/issue/{issueIdOrKey}/watchers [GET, POST, DELETE]
- * http://example.com:8080/jira/rest/api/2/issue/{issueIdOrKey}/worklog [GET, POST]
- * http://example.com:8080/jira/rest/api/2/issue/{issueIdOrKey}/worklog/{id} [GET, PUT, DELETE]
- * http://example.com:8080/jira/rest/api/2/issue/createmeta [GET]
- * http://example.com:8080/jira/rest/api/2/issue/picker [GET]
  */
 class IssueEndpoint extends AbstractEndpoint
 {
